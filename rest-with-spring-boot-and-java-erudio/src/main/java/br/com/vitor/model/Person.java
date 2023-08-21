a70ac3a8a85a72def906bac21994cf625951a852
limavit <limavit@gmail.com>
2023-08-21 15:03:51 -0300

    implementando jpa

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/vitor/model/Person.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/vitor/model/Person.java
@@ -3,13 +3,28 @@ package br.com.vitor.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="person")
 public class Person implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
+	@Column(name = "last_name", nullable = false)
 	private String lastName;
+	@Column(nullable = false)
 	private String address;
+	@Column(nullable = false)
 	private String gender;
 	
 	
