cd1ab2f876ab9e450bf6c9b80e09ed33a42fbf1a
limavit <limavit@gmail.com>
2023-09-08 12:58:06 -0300

    implementing flyway migrations

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V1__add_new_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS t_person (
+  id bigint NOT NULL AUTO_INCREMENT,
+  address varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  gender varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__insert_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO t_person (id, address, first_name, gender, last_name) VALUES
+	(1, 'Araraquara - São Paulo - Brasil', 'Vitor', 'Male', 'Lima'),
+	(4, 'SP - São Paulo - Brasil', 'Rodrigo', 'Male', 'da Silva'),
+	(6, 'SP - São Paulo - Brasil', 'Teste versionamento', 'Male', 'V2'),
+	(7, 'SP - São Paulo - Brasil', 'Teste versionamento V1', 'Male', 'V1');
+
