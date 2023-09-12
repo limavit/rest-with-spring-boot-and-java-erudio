cc337a020e1c585209c0c3c8d5bc967f781cff02
limavit <limavit@gmail.com>
2023-09-12 15:31:44 -0300

    content negotiation header param

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/vitor/config/WebConfig.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/vitor/config/WebConfig.java
@@ -10,9 +10,19 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		/* Query param
 		configurer.favorParameter(true)
 		.parameterName("mediaType")
 		.ignoreAcceptHeader(true)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+		// Header param
+		configurer.favorParameter(false)
+		.ignoreAcceptHeader(false)
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
