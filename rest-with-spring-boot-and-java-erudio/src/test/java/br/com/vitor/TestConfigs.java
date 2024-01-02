08fb9735c008acbb1662bdc7c4fed11b1c39b49d
limavit <limavit@gmail.com>
2024-01-02 14:54:07 -0300

    TestContainers e rest-assured

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/vitor/TestConfigs.java
@@ -0,0 +1,8 @@
+package br.com.vitor;
+
+public class TestConfigs {
+	
+	public static final int SERVER_PORT = 8888;
+	
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/vitor/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,30 @@
+package br.com.vitor.integrationtests.swagger;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
+
+import br.com.vitor.TestConfigs;
+import br.com.vitor.integrationtests.testcontainers.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest{
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
